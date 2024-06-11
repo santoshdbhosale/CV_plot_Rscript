5ec93265716a75616fdd65e90f584053dd3868aa
Santosh <santoshdbhosale@gmail.com>
2024-06-11 14:34:48 -0700

    updated

---
+++ Bar_plot/barplot_errorbar.R
@@ -0,0 +1,20 @@
+esquisser()
+
+library(ggplot2)
+bar =read.delim('barP.tsv')
+p = ggplot(bar) +
+ aes(x = Samples, y = Protein_groups, fill = Reprod) +
+ geom_col() +
+ scale_fill_hue(direction = 1) +
+ labs(x = "Daywise reproducibility", y = "# Protein groups", title = "# Protein groups IDs summary", 
+ subtitle = "Interday reproducibility", fill = "Reprod") +
+ theme_gray() +
+ theme(plot.title = element_text(size = 14L, 
+ face = "bold", hjust = 0.5), plot.subtitle = element_text(size = 12L, face = "bold"), axis.title.y = element_text(size = 12L, 
+ face = "bold"), axis.title.x = element_text(size = 12L, face = "bold")) +
+ facet_wrap(vars(Reprod), 
+ scales = "free_x")
+p
+p1 = p + geom_errorbar(aes(ymin=Protein_groups-Std_dev, ymax=Protein_groups+Std_dev), width=0.2,
+                       position=position_dodge(2))
+p1
